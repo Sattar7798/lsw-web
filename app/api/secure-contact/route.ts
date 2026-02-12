bb287061c83e522dd1cbbb470577a92a73223003
Sattar H <sattarhedayat2020@gmail.com>
2026-02-12 13:10:58 +0100

    4

--- app/api/secure-contact/route.ts
+++ app/api/secure-contact/route.ts
@@ -3,9 +3,9 @@ import { Resend } from 'resend';
 
 export const runtime = 'edge';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     try {
         const { encryptedMessage, encryptedAttachments } = await request.json();
 
